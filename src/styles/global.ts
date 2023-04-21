4a9b7cba81d40ee5f0997d029948a6158acff00d
Luciana Santos <github@luciana.dev>
2023-04-21 16:43:28 -0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { globalCss } from './stitches.config'
+
+export const globalStyles = globalCss({
+  '*, *::before, *::after': {
+    margin: 0,
+    padding: 0,
+    boxSizing: ' border-box',
+  },
+
+  'h1, h2, h3': {
+    lineHeight: 1,
+    fontWeight: 400,
+  },
+
+  a: {
+    textDecoration: 'none',
+  },
+
+  ul: {
+    listStyle: 'none',
+  },
+
+  body: {
+    minHeight: '100vh',
+    lineHeight: '1.5',
+    backgroundColor: '$black500',
+    fontFamily: 'Montserrat, sans-serif',
+    color: '$white',
+  },
+})
---
+++ src/styles/stitches.config.ts
@@ -0,0 +1,51 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      accent: '#9580FF',
+      white200: '#E8E8E8',
+      white: '#FFFFFF',
+      black800: '#15171B',
+      black500: '#21222C',
+      gray600: '#414558',
+      gray400: '#626784',
+      gray200: '#B6B9C9',
+    },
+    space: {
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      10: '2.5rem',
+      12: '3rem',
+      16: '4rem',
+      20: '5rem',
+      40: '10rem',
+      64: '16rem',
+      80: '20rem',
+    },
+    fontSizes: {
+      xxs: '0.625rem',
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '4xl': '2rem',
+    },
+  },
+})
